31bbfbbf632578d90fcb02f4af1073f50ee300c0
RodNSS <jamesroderick23@gmail.com>
2023-02-14 21:31:35 -0600

    did some bonus questions

--- RM_sql_lahman.sql
+++ RM_sql_lahman.sql
@@ -63,11 +63,11 @@ LIMIT 1;
 
 -- 5. From 1970 to 2016, what is the largest number of wins for a team that did not win the world series? What is the smallest number of wins for a team that did win the world series?  
 
-SELECT 
+SELECT
   MAX(CASE WHEN wswin = 'N' THEN w END) AS most_wins_no_ws,
   MIN(CASE WHEN wswin = 'Y' THEN w END) AS least_wins_ws
 FROM teams
-WHERE yearid BETWEEN 1970 AND 2016;
+WHERE yearid BETWEEN 1970 AND 2016
 
 -- 116 is the largest number of wins for a team that did not win the world series while 63 is the smallest number of wins for a team that did win the world series. 
 
@@ -135,12 +135,12 @@ SELECT namefirst,
        SUM(so) AS strikeouts,
        salary::numeric::money AS total_salary,
        salary::numeric::money / SUM(so) AS so_cost
-FROM pitching
-LEFT JOIN salaries
+FROM pitching p
+LEFT JOIN salaries AS s
 	USING(playerid)
 LEFT JOIN people
 	USING(playerid)
-WHERE pitching.yearid = 2016 AND gs >= 10 AND salaries.yearid = 2016
+WHERE p.yearid = 2016 AND gs >= 10 AND s.yearid = 2016
 GROUP BY total_salary, namefirst, namelast
 ORDER BY so_cost DESC;
 
@@ -164,25 +164,16 @@ ORDER BY total_hits DESC;
 
 -- 9. Find all players who had at least 1,000 hits for two different teams. Report those players' full names.
 
-SELECT namefirst,
-	   namelast, 
-	   SUM(h) AS total_hits
-FROM batting
-LEFT JOIN people
-	USING(playerid)
-GROUP BY namefirst, namelast
-HAVING count(distinct teamID) = 2 AND sum(h) >= 1000;
-
 WITH two_teams AS (
-	SELECT playerID, teamID, sum(h) as total_hits
+	SELECT playerID, teamID, sum(h) AS total_hits
 	FROM batting
 	GROUP BY playerID, teamID
-	HAVING sum(h) >= 1000)
-SELECT namefirst, namelast
+	HAVING SUM(h) >= 1000)
+SELECT namefirst || ' ' || namelast AS name
 FROM two_teams
 LEFT JOIN people 
 USING(playerID)
-GROUP BY playerid, namefirst, namelast
+GROUP BY playerid, name
 HAVING COUNT(DISTINCT teamID) = 2;
 
 -- 10. Find all players who hit their career highest number of home runs in 2016. Consider only players who have played in the league for at least 10 years, and who hit at least one home run in 2016. Report the players' first and last names and the number of home runs they hit in 2016.
@@ -289,3 +280,196 @@ ORDER BY avg_attendance_boost DESC;
 -- Teams that win the world series see an average attendance boost the following year of 16,482. Whereas teams that are wild card winners see an average boost of 97,548. Division winners - 22,233 and no championship win - 12,660.
 
 -- 13. It is thought that since left-handed pitchers are more rare, causing batters to face them less often, that they are more effective. Investigate this claim and present evidence to either support or dispute this claim. First, determine just how rare left-handed pitchers are compared with right-handed pitchers. Are left-handed pitchers more likely to win the Cy Young Award? Are they more likely to make it into the hall of fame?
+
+WITH pitchers AS (
+  SELECT 
+    CASE WHEN throws = 'L' THEN 'Left' ELSE 'Right' END AS throwing_arm,
+    COUNT(playerID) AS total_pitchers
+  FROM people
+  GROUP BY throwing_arm
+)
+SELECT throwing_arm, 
+       total_pitchers, 
+       ROUND(total_pitchers * 100.0 / SUM(total_pitchers) OVER ()) AS percentage
+FROM pitchers;
+
+-- 19% of pitchers are left handed.
+
+-- Bonus Questions
+
+-- 1. In this question, you'll get to practice correlated subqueries and learn about the LATERAL keyword. Note: This could be done using window functions, but we'll do it in a different way in order to revisit correlated subqueries and see another keyword - LATERAL.
+
+-- a. First, write a query utilizing a correlated subquery to find the team with the most wins from each league in 2016.
+
+-- If you need a hint, you can structure your query as follows:
+
+-- SELECT DISTINCT lgid, ( <Write a correlated subquery here that will pull the teamid for the team with the highest number of wins from each league> )
+-- FROM teams t
+-- WHERE yearid = 2016;
+
+SELECT DISTINCT lgid, 
+    (SELECT teamid FROM teams t2 
+     WHERE t.lgid = t2.lgid AND t2.yearid = 2016 
+     ORDER BY t2.w DESC LIMIT 1) AS highest_wins
+FROM teams t
+WHERE yearid = 2016;
+
+SELECT DISTINCT lgid, 
+    (SELECT name FROM teams t2 
+     WHERE t.lgid = t2.lgid AND t2.yearid = 2016 
+     ORDER BY t2.w DESC LIMIT 1) AS highest_wins
+FROM teams t
+WHERE yearid = 2016;
+
+-- Texas Rangers in AL and Chicago Cubs in NL
+
+-- b. One downside to using correlated subqueries is that you can only return exactly one row and one column. This means, for example that if we wanted to pull in not just the teamid but also the number of wins, we couldn't do so using just a single subquery. (Try it and see the error you get). Add another correlated subquery to your query on the previous part so that your result shows not just the teamid but also the number of wins by that team.
+
+SELECT DISTINCT lgid, 
+    (SELECT name FROM teams t2 
+     WHERE t.lgid = t2.lgid AND t2.yearid = 2016 
+     ORDER BY t2.w DESC LIMIT 1) AS highest_wins,
+	 (SELECT w FROM teams t3 
+     WHERE t.lgid = t3.lgid AND t3.yearid = 2016 AND t.w = t3.w 
+     ORDER BY t3.l DESC LIMIT 1) AS wins
+FROM teams t
+WHERE yearid = 2016;
+
+-- c. If you are interested in pulling in the top (or bottom) values by group, you can also use the DISTINCT ON expression (https://www.postgresql.org/docs/9.5/sql-select.html#SQL-DISTINCT). Rewrite your previous query into one which uses DISTINCT ON to return the top team by league in terms of number of wins in 2016. Your query should return the league, the teamid, and the number of wins.
+
+SELECT DISTINCT ON(lgid) lgid, teamid, w
+FROM teams
+WHERE yearid = 2016
+ORDER BY lgid, w DESC;
+
+
+-- d. If we want to pull in more than one column in our correlated subquery, another way to do it is to make use of the LATERAL keyword (https://www.postgresql.org/docs/9.4/queries-table-expressions.html#QUERIES-LATERAL). This allows you to write subqueries in FROM that make reference to columns from previous FROM items. This gives us the flexibility to pull in or calculate multiple columns or multiple rows (or both). Rewrite your previous query using the LATERAL keyword so that your result shows the teamid and number of wins for the team with the most wins from each league in 2016. 
+
+-- If you want a hint, you can structure your query as follows:
+
+-- SELECT *
+-- FROM (SELECT DISTINCT lgid 
+-- 	  FROM teams
+-- 	  WHERE yearid = 2016) AS leagues,
+-- 	  LATERAL ( <Fill in a subquery here to retrieve the teamid and number of wins> ) as top_teams;
+
+SELECT *
+FROM (SELECT DISTINCT lgid 
+      FROM teams
+      WHERE yearid = 2016) AS leagues,
+      LATERAL (
+       SELECT teamid, w
+       FROM teams t2
+       WHERE t2.lgid = leagues.lgid AND t2.yearid = 2016
+       ORDER BY w DESC
+       LIMIT 1
+      ) AS top_teams;
+	  
+-- e. Finally, another advantage of the LATERAL keyword over using correlated subqueries is that you return multiple result rows. (Try to return more than one row in your correlated subquery from above and see what type of error you get). Rewrite your query on the previous problem sot that it returns the top 3 teams from each league in term of number of wins. Show the teamid and number of wins.
+
+SELECT *
+FROM (SELECT DISTINCT lgid 
+      FROM teams
+      WHERE yearid = 2016) AS leagues,
+      LATERAL (
+       SELECT teamid, w
+       FROM teams t2
+       WHERE t2.lgid = leagues.lgid AND t2.yearid = 2016
+       ORDER BY w DESC
+       LIMIT 3
+      ) AS top_teams;
+
+-- AL - TEX, CLE, BOS
+-- NL - CHN, WAS, LAN
+
+-- 2. Another advantage of lateral joins is for when you create calculated columns. In a regular query, when you create a calculated column, you cannot refer it it when you create other calculated columns. This is particularly useful if you want to reuse a calculated column multiple times. For example,
+
+-- SELECT 
+-- 	teamid,
+-- 	w,
+-- 	l,
+-- 	w + l AS total_games,
+-- 	w*100.0 / total_games AS winning_pct
+-- FROM teams
+-- WHERE yearid = 2016
+-- ORDER BY winning_pct DESC;
+
+-- results in the error that "total_games" does not exist. However, I can restructure this query using the LATERAL keyword.
+
+-- SELECT
+-- 	teamid,
+-- 	w,
+-- 	l,
+-- 	total_games,
+-- 	w*100.0 / total_games AS winning_pct
+-- FROM teams t,
+-- LATERAL (
+-- 	SELECT w + l AS total_games
+-- ) AS tg
+-- WHERE yearid = 2016
+-- ORDER BY winning_pct DESC;
+
+-- a. Write a query which, for each player in the player table, assembles their birthyear, birthmonth, and birthday into a single column called birthdate which is of the date type.
+
+SELECT namefirst,
+       namelast,
+       (birthyear || '-' || birthmonth || '-' || birthday)::date AS birthdate
+FROM people;
+
+-- b. Use your previous result inside a subquery using LATERAL to calculate for each player their age at debut and age at retirement. (Hint: It might be useful to check out the PostgreSQL date and time functions https://www.postgresql.org/docs/8.4/functions-datetime.html).
+
+SELECT namefirst,
+       namelast,
+       (birthyear || '-' || birthmonth || '-' || birthday)::date AS birthdate,
+EXTRACT(YEAR FROM age(debut::date, (birthyear || '-' || birthmonth || '-' || birthday)::date)) AS debut_age,
+EXTRACT(YEAR FROM age(finalgame::date, (birthyear || '-' || birthmonth || '-' || birthday)::date)) AS retirement_age
+FROM people
+ORDER BY birthdate;
+
+-- c. Who is the youngest player to ever play in the major leagues?
+
+SELECT namefirst,
+       namelast,
+       (birthyear || '-' || birthmonth || '-' || birthday)::date AS birthdate,
+EXTRACT(YEAR FROM age(debut::date, (birthyear || '-' || birthmonth || '-' || birthday)::date)) AS debut_age,
+EXTRACT(YEAR FROM age(finalgame::date, (birthyear || '-' || birthmonth || '-' || birthday)::date)) AS retirement_age
+FROM people
+ORDER BY debut_age
+LIMIT 1;
+
+-- Joe Nuxhall at age 15
+
+-- d. Who is the oldest player to player in the major leagues? You'll likely have a lot of null values resulting in your age at retirement calculation. Check out the documentation on sorting rows here https://www.postgresql.org/docs/8.3/queries-order.html about how you can change how null values are sorted.
+
+SELECT namefirst,
+       namelast,
+       (birthyear || '-' || birthmonth || '-' || birthday)::date AS birthdate,
+EXTRACT(YEAR FROM age(debut::date, (birthyear || '-' || birthmonth || '-' || birthday)::date)) AS debut_age,
+EXTRACT(YEAR FROM age(finalgame::date, (birthyear || '-' || birthmonth || '-' || birthday)::date)) AS retirement_age
+FROM people
+ORDER BY retirement_age DESC NULLS LAST
+LIMIT 1;
+
+-- Satchel Paige at 59 years old.
+
+-- 3. For this question, you will want to make use of RECURSIVE CTEs (see https://www.postgresql.org/docs/13/queries-with.html). The RECURSIVE keyword allows a CTE to refer to its own output. Recursive CTEs are useful for navigating network datasets such as social networks, logistics networks, or employee hierarchies (who manages who and who manages that person). To see an example of the last item, see this tutorial: https://www.postgresqltutorial.com/postgresql-recursive-query/. 
+-- In the next couple of weeks, you'll see how the graph database Neo4j can easily work with such datasets, but for now we'll see how the RECURSIVE keyword can pull it off (in a much less efficient manner) in PostgreSQL. (Hint: You might find it useful to look at this blog post when attempting to answer the following questions: https://data36.com/kevin-bacon-game-recursive-sql/.)
+
+-- a. Willie Mays holds the record of the most All Star Game starts with 18. How many players started in an All Star Game with Willie Mays? (A player started an All Star Game if they appear in the allstarfull table with a non-null startingpos value).
+
+SELECT COUNT(DISTINCT playerid) AS total_starters
+FROM allstarfull 
+WHERE gameid IN (
+    SELECT gameid
+    FROM allstarfull 
+    WHERE playerid = 'mayswi01'
+)
+AND startingpos IS NOT NULL;
+
+-- 165 players
+
+-- b. How many players didn't start in an All Star Game with Willie Mays but started an All Star Game with another player who started an All Star Game with Willie Mays? For example, Graig Nettles never started an All Star Game with Willie Mayes, but he did star the 1975 All Star Game with Blue Vida who started the 1971 All Star Game with Willie Mays.
+
+-- c. We'll call two players connected if they both started in the same All Star Game. Using this, we can find chains of players. For example, one chain from Carlton Fisk to Willie Mays is as follows: Carlton Fisk started in the 1973 All Star Game with Rod Carew who started in the 1972 All Star Game with Willie Mays. Find a chain of All Star starters connecting Babe Ruth to Willie Mays. 
+
+-- d. How large a chain do you need to connect Derek Jeter to Willie Mays?
